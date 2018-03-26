6c773dfa3e5598170168e789e2a37ef7ecc43709
James Bonnyman <100559364@oncampus.local>
2018-03-26 14:21:20 -0400

    [Add] New file to be formula

---
+++ Lab5/Formula.js
@@ -0,0 +1,36 @@
+var chart;
+var height = 200
+var width = 300
+//DEFINE YOUR VARIABLES UP HERE
+
+
+//Gets called when the page is loaded.
+function init(){
+
+
+  //
+}
+
+//Called when the update button is clicked
+function updateClicked(){
+  d3.csv('data/CoffeeData.csv',update)
+}
+
+//Callback for when data is loaded
+function update(data){
+  
+
+
+  //
+}
+
+// Returns the selected option in the X-axis dropdown. Use d[getXSelectedOption()] to retrieve value instead of d.getXSelectedOption()
+function getXSelectedOption(){
+ 
+}
+
+// Returns the selected option in the X-axis dropdown. 
+function getYSelectedOption(){
+  
+}
+
