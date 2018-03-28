e97dc0fdcc43782cee454933baa423a2ad11425d
James <james.bonnyman@uoit.net>
2018-03-28 15:28:35 -0400

    [Add] outlines of formula code. needs a data source, needs help with syntax

--- Lab5/Formula.js
+++ Lab5/Formula.js
@@ -17,8 +17,32 @@ function updateClicked(){
 }
 
 //Callback for when data is loaded
-function update(data){
+function update(data)
+{
+    /*
+  I will Need: 
+  - a data class that stores 
+    - the min
+    - the max
+    - an array of the data (doesn't need to have country labels) 
+    - the current weight of it 
+
+   - I will need to be passed 
+    - a vector of the data class so that I know how many i am dealing with here.     
+  */
+
   
+   // for (int i =0; i < StorageVector.length; i++)         // iterates through the array of classes
+    {
+     //   for (int j = 0; j < storageVector[i].array.length; j++)       // iterates through the internal array of the class
+        { 
+        // result +=(( StorageVector[i].Array[j] -  StorageVector[i].min) / ( StorageVector[i].max -  StorageVector[i].min ))*  StorageVector[i].weight;
+        }
+
+    }
+    
+    
+
 
 
   //
