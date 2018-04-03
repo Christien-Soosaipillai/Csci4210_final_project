c951904c7c7a6eee2064306deed05ebf2946049d
Christien-Soosaipillai <christien2011@hotmail.ca>
2018-04-02 23:04:30 -0400

    changes to js file and parser

--- Parse/src/parser.java
+++ Parse/src/parser.java
@@ -5,18 +5,25 @@ import java.util.Scanner;
 public class parser{
 
 
+    //File Names
+    //gdp-per-capita-in-international-and-market-dollars-clean.csv
+    //life-expectancy-clean.csv
+    //total-healthcare-expenditure-as-share-of-national-gdp-by-country-clean.csv
+    //WHOSIS_000004-clean.csv
+
     public static void main(String[] args)throws IOException {
 
         //Read file
-        String filename = "/home/christien/Documents/School/Parse/src/data/Original-Data/total-healthcare-expenditure-as-share-of-national-gdp-by-country.csv";
+        String filename = "/home/christien/Documents/School/Parse/src/data/Original-Data/cleandata.csv";
 
         //Write file
 
-        File file = new File("src/data/Clean/total-healthcare-expenditure-as-share-of-national-gdp-by-country-clean.csv");
+        File file = new File("src/data/Clean/cleandata-clean.csv");
+
 
 
         //country list
-        String[] countries = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands", "Virgin Islands", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"};
+        String[] countries = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos Islands", "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands", "Virgin Islands", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"};
 
 
         try{
@@ -32,7 +39,23 @@ public class parser{
             //Delimiter
             String comma = ",";
             String newLine = "\n";
-            String File_Header =  "Entity,Code,Year, (% of GDP)";
+            String File_Header =  "Entity,Year,Life expectancy";
+
+            //gdp us
+            double GDP_US_MIN = 195.7;
+            double GDP_US_MAX = 196.0;
+
+            //gdp international
+            double GDP_Inter_MIN = 923.7;
+            double GDP_Inter_MAX = 924.7;
+
+            //life expectancy
+            double life_Exp_Min = 58.8;
+            double life_Exp_Max = 59.8;
+
+            //health care percent
+            double health_min = 6.9;
+            double health_max = 6.9;
 
 
             String[] values = null;
@@ -70,21 +93,65 @@ public class parser{
                                 if(values[2] != null) {
                                     f1Writer.append(values[2]);
                                     f1Writer.append(comma);
+
                                 }
 
                                 if(values.length > 3 && values[3] != null){
                                     f1Writer.append(values[3]);
                                     f1Writer.append(comma);
+
+                                    if(Double.parseDouble(values[3]) > GDP_US_MAX ){
+                                        GDP_US_MAX = Double.parseDouble(values[3]);
+
+                                    }
+                                    if(Double.parseDouble(values[3]) < GDP_US_MIN){
+                                        GDP_US_MIN = Double.parseDouble(values[3]);
+
+                                    }
+
                                 }
 
                                 if(values.length > 4 && values[4] != null){
                                     f1Writer.append(values[4]);
                                     f1Writer.append(comma);
+
+                                    if(Double.parseDouble(values[4]) > GDP_Inter_MAX ){
+                                        GDP_Inter_MAX = Double.parseDouble(values[4]);
+
+                                    }
+                                    if(Double.parseDouble(values[4]) < GDP_Inter_MIN){
+                                        GDP_Inter_MIN = Double.parseDouble(values[4]);
+
+                                    }
+
                                 }
 
                                 if(values.length > 5 && values[5] != null){
                                     f1Writer.append(values[5]);
                                     f1Writer.append(newLine);
+
+                                    if(Double.parseDouble(values[5]) > life_Exp_Max){
+                                        life_Exp_Max = Double.parseDouble(values[5]);
+
+                                    }
+                                    if(Double.parseDouble(values[5]) < life_Exp_Min){
+                                        life_Exp_Min = Double.parseDouble(values[5]);
+
+                                    }
+                                }
+
+                                if(values.length > 6 && values[6] != null){
+                                    f1Writer.append(values[6]);
+                                    f1Writer.append(newLine);
+
+                                    if(Double.parseDouble(values[6]) > health_max){
+                                        health_max = Double.parseDouble(values[6]);
+
+                                    }
+                                    if(Double.parseDouble(values[6]) < health_min){
+                                        health_min = Double.parseDouble(values[6]);
+
+                                    }
                                 }
 
                                 f1Writer.append(newLine);
@@ -101,6 +168,15 @@ public class parser{
 
             }
             System.out.println(count+" countries were found for that year");
+            System.out.println("GDP_US_MIN=" + GDP_US_MIN );
+            System.out.println("GDP_US_MAX=" + GDP_US_MAX );
+            System.out.println("GDP_INTER_MIN=" + GDP_Inter_MIN );
+            System.out.println("GDP_INTER_MIN=" + GDP_Inter_MAX );
+            System.out.println("life min=" + life_Exp_Min );
+            System.out.println("life max=" + life_Exp_Max );
+            System.out.println("health care min=" + health_min);
+            System.out.println("health care max=" + health_max);
+
             f1Writer.close();
             scanner.close();
 
--- Project_Folder/Formula.js
+++ Project_Folder/Formula.js
@@ -1,21 +1,103 @@
 var chart;
-var height = 200
-var width = 300
+var margin = {top: 60, right: 100, bottom: 70, left: 80},
+  width = 850 - margin.left - margin.right,
+  height = 370 - margin.top - margin.bottom;
+var svg;
 //DEFINE YOUR VARIABLES UP HERE
 
 
+//java parsed values
+// GDP_US_MIN=112.8493703
+// GDP_US_MAX=115761.5077
+// GDP_INTER_MIN=0.0
+// GDP_INTER_MIN=129349.9164
+// life min=41.668999
+// life max=83.480003
+// health care min=1.63062912
+// health care max=53.0
+
+//GDP US values
+var gdp_Us_min;
+var gdp_Us_max;
+
+//GDP International values
+var gdp_national_min;
+var gdp_national_max;
+
+//Life expectancy values
+var life_exp_min;
+var life_exp_max;
+
+//Health Precentage values
+var health_prec_min;
+var health_prec_max;
+
+
+
 //Gets called when the page is loaded.
-function init(){
+
+  svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-  //
-}
+
+// setup x 
+var xValue = function(d) { return d.gdpus;}, // data -> value
+    xScale = d3.scale.linear().range([0, width]), // value -> display
+    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+// setup y
+var yValue = function(d) { return d.lifexp;}, // data -> value
+    yScale = d3.scale.linear().range([height, 0]), // value -> display
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
 
 //Called when the update button is clicked
 function updateClicked(){
-  d3.csv('data/Clean-3/WHOSIS_000004-clean.csv',function(data))
-  {
-  }
+  d3.csv('data/Clean-3/cleandata.csv',function(data){
+        
+      //Sort by country
+      var country_data_sort = d3.nest()
+                                .key(function(d){return d.country;})
+                                .entries(data);  
+      
+  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+  yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+  // x-axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Calories");
+
+  // y-axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Protein (g)");
+
+    
+      console.log(country_data_sort);
+      
+      });
+  
 }
 
 //Callback for when data is loaded
@@ -35,13 +117,13 @@ function update(data)
 
   
    // for (int i =0; i < StorageVector.length; i++)         // iterates through the array of classes
-    {
+    //{
      //   for (int j = 0; j < storageVector[i].array.length; j++)       // iterates through the internal array of the class
-        { 
+        //{ 
         // result +=(( StorageVector[i].Array[j] -  StorageVector[i].min) / ( StorageVector[i].max -  StorageVector[i].min ))*  StorageVector[i].weight;
-        }
+        //}
 
-    }
+    //}
     
     
 
